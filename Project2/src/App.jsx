93c90e520763a196739074691b31a9bd066cc466
Vaibhav Shukla <shuklavaibhav267@gmail.com>
2024-06-22 17:06:48 +0530

    Interview Prep

--- Project2/src/App.jsx
+++ Project2/src/App.jsx
@@ -6,34 +6,52 @@ import './App.css'
 
 
 function App(){
+
+let [Counter,SetCounter]=useState(15)
+const addValue=()=>{
+  // Interview question in counter.....that what happens if we dulpicate the below setCounter 
+  // Counter=Counter+1;
+  SetCounter(Counter+1)
+  // SetCounter(Counter+1)
+  // SetCounter(Counter+1)
+  // SetCounter(Counter+1)
+  // SetCounter(Counter+1)
+  // Answer is : isme setCounter ka batch ban jyga aur ke hi kaam ko repeat krta hai so no effect 
+
+  SetCounter(prevCounter=>prevCounter+1)
+  SetCounter(prevCounter=>prevCounter+1)
+  SetCounter(prevCounter=>prevCounter+1)
+  SetCounter(prevCounter=>prevCounter+1)
+
+
+  // Isme hamra previous state ata hai 
+  // Ab jab ham click krenge button me to value +4 add hogi aur value-4 Delete hogi
+  // so kya kuch aesa ho skata hai jisme 
+  // Note:> SetCounter ke andar hame basically call back milta hai
   
-  let [counter,setCounter]=useState(15)
+}
+const delValue=()=>{
+  // Counter=Counter-1;
+  SetCounter(Counter-1)
+  SetCounter(prevCounter=>prevCounter-1)
+  SetCounter(prevCounter=>prevCounter-1)
+  SetCounter(prevCounter=>prevCounter-1)
+  SetCounter(prevCounter=>prevCounter-1)
+}
 
-  const addValue=()=>{
-    counter=counter+1
-    setCounter(counter)
-  }
-
-  const delValue=()=>{
-    counter=counter-1
-    setCounter(counter)
-  }
 
   return(
     <>
-    <h1>Learning React Using Hooks</h1>
-    <h2>Counter Count:{counter}</h2>
+    <h1>React And Hooks</h1>
+    <h2>Counter Count:{Counter}</h2>
     <button 
-    onClick={addValue}>Add Value:{counter}</button>
+    onClick={addValue}>Add Value:{Counter}</button>
     <br />
     <br />
     <button
-    onClick={delValue}>Reduce Value:{counter}</button>
-    <p>footer:{counter}</p>
-    
-    
+    onClick={delValue}>Delete Value:{Counter}</button>
+    <p>Footer : {Counter}</p>
     </>
   )
 }
-
 export default App
