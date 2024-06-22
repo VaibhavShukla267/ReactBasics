b79ab9fc311af024d68ce95c45964203cfa768d5
Vaibhav Shukla <shuklavaibhav267@gmail.com>
2024-06-22 11:55:15 +0530

    Used Props and Tailwind

---
+++ Project3/src/App.jsx
@@ -0,0 +1,32 @@
+// import { useState } from 'react'
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+// Passing One value to other components
+  // let obj={
+  //   username:"Vaibhav",
+  //   Age:22
+  // }
+  // let Arr=[1,23,4]
+
+
+
+  return ( 
+<>
+
+  <h1 className='bg-blue-400 text-black rounded-xl mb-3 p-3'>Tailwind In React</h1>
+  {/* /* <Card Username="MyName" someObj={Arr} /> */ }
+  <Card Username="MyName" btnName="Click Explore More" />
+  <Card Username="Vaibhav" btnName="Click Here To See Me"/>
+  
+   {/* Yaha pe har card apni ek alag value rakhega uska intezam props se hota hai */}
+
+
+</>
+  ) 
+
+}
+
+export default App
